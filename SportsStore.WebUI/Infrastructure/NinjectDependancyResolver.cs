3feca950a6e8a9a1a5d20886e2e55608165187bf
Amelia Marrone <68938110+ameliamaedae@users.noreply.github.com>
2025-01-28 10:57:40 -0600

    Day Two

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependancyResolver.cs
@@ -0,0 +1,35 @@
+﻿using Ninject;
+using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependancyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependancyResolver (IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBinding();
+        }
+
+        public object GetService(Type myserviceType) 
+        { 
+            return kernel.GetService(myserviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type myserviceType) 
+        { 
+            return kernel.GetAll(myserviceType);
+        }
+
+        private void AddBinding() 
+        {
+            // Put bindings here
+        }
+    }
+}
